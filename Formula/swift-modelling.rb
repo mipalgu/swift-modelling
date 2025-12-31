a0a6fe834dd6df207063da731b17ad101e8f1d3a
Rene Hexel <r.hexel@griffith.edu.au>
2026-01-01 08:53:39 +1000

    Add HomeBrew formula.

---
+++ Formula/swift-modelling.rb
@@ -0,0 +1,21 @@
+class SwiftModelling < Formula
+  desc "A CLI wrapper for the Swift Modelling Framework (EMF, ATL, MTL)"
+  homepage "https://github.com/mipalgu/swift-modelling"
+  license "MIT"
+  head "https://github.com/mipalgu/swift-modelling.git", branch: "main"
+
+  depends_on :xcode => ["16.0", :build]
+
+  def install
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/swift-ecore"
+    bin.install ".build/release/swift-atl"
+    bin.install ".build/release/swift-mtl"
+  end
+
+  test do
+    system "#{bin}/swift-ecore", "--help"
+    system "#{bin}/swift-atl", "--help"
+    system "#{bin}/swift-mtl", "--help"
+  end
+end
