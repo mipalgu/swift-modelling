ebbe46e0f4fa2b450b94496b3379b1a8bbfb0a31
Rene Hexel <r.hexel@griffith.edu.au>
2026-01-01 09:00:46 +1000

    Tap swift-modelling v0.1.0

--- Formula/swift-modelling.rb
+++ Formula/swift-modelling.rb
@@ -1,6 +1,8 @@
 class SwiftModelling < Formula
   desc "A CLI wrapper for the Swift Modelling Framework (EMF, ATL, MTL)"
   homepage "https://github.com/mipalgu/swift-modelling"
+  url "https://github.com/mipalgu/swift-modelling/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "b1439fa9c9f824c11eda8bdde29f4445374321f3f7601553055694d113df6661"
   license "MIT"
   head "https://github.com/mipalgu/swift-modelling.git", branch: "main"
 
