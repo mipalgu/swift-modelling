759fb1f08b2d7e94d0f98ab1410025b00c0a0ae3
Rene Hexel <r.hexel@griffith.edu.au>
2026-02-19 12:15:37 +1000

    Fix version test to expect 0.1.8

--- Tests/swift-mtl-tests/CLIIntegrationTests.swift
+++ Tests/swift-mtl-tests/CLIIntegrationTests.swift
@@ -521,7 +521,7 @@ struct CLIIntegrationTests {
 
         // Then
         #expect(result.succeeded)
-        #expect(result.stdout.contains("0.1.7"))
+        #expect(result.stdout.contains("0.1.8"))
     }
 
     @Test("Generate help")
